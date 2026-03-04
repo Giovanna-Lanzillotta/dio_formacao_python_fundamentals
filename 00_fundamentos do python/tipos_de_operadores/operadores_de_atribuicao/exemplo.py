424c07ea6348825342ec9062412bb153b08d2cfd
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-03-03 21:02:30 -0300

    operadores de atribuicao

---
+++ 00_fundamentos do python/tipos_de_operadores/operadores_de_atribuicao/exemplo.py
@@ -0,0 +1,50 @@
+# ATRIBUIÇÃO SIMPLES
+
+saldo = 500
+print(f'atribuição simples: {saldo}')
+
+
+# ATRIBUIÇÃO COM ADIÇÃO
+
+saldoAdicao = 500
+saldoAdicao += 200
+print(f'atribuição com adição: {saldoAdicao}')
+
+
+# ATRIBUIÇÃO COM SUBTRAÇÃO
+
+saldoSubtracao = 500
+saldoSubtracao -= 200
+print(f'atribuição com subtração: {saldoSubtracao}')
+
+
+# ATRIBUIÇÃO COM MULTIPLICAÇÃO
+
+saldoMultiplicacao = 500
+saldoMultiplicacao *= 2
+print(f'atribuição com multiplicacao: {saldoMultiplicacao}')
+
+
+# ATRIBUIÇÃO COM DIVISÃO
+
+saldoDivisao = 500
+saldoDivisao /= 5
+print(f'atribuição com divisão: {saldoDivisao}')
+
+saldoDivisaoInteira = 500
+saldoDivisaoInteira //= 5
+print(f'atribuição com divisão inteira: {saldoDivisaoInteira}')
+
+
+# ATRIBUIÇÃO COM MÓDULO
+
+saldoModulo = 500
+saldoModulo %= 480
+print(f'atribuição com módulo: {saldoModulo}')
+
+
+# ATRIBUIÇÃO COM EXPONENCIAÇÃO
+
+saldoModulo = 80
+saldoModulo **= 2
+print(f'atribuição com exponenciação: {saldoModulo}')
---
+++ 00_fundamentos do python/tipos_de_operadores/operadores_de_atribuicao/operadores_atribuicao.py
@@ -0,0 +1,25 @@
+saldo = 200
+print(f"o saldo é de {saldo}")
+
+
+saldo += 10
+print(f"o saldo somou 10 e agora é {saldo}")
+
+saldo -= 5
+print(f"o saldo diminui 5 e agora é {saldo}")
+
+saldo //= 2
+print(f"o saldo dividiu inteiramente por 2 e agora é {saldo}")
+
+saldo /= 2
+print(f"o saldo dividiu por 2 e agora é {saldo}")
+
+
+saldo *= 10
+print(f"o saldo multiplicou por 10 e agora é {saldo}")
+
+saldo %= 4
+print(f"o seu módulo é {saldo}")
+
+saldo **= 2
+print(f"o saldo elevado por 2 é {saldo}")
