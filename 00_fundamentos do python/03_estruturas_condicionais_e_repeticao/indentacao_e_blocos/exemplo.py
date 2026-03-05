8fc51eea22593f05a2a57fbb6668a61957f280c4
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-03-05 11:18:27 -0300

    atualizações + indentação e blocos

---
+++ 00_fundamentos do python/03_estruturas_condicionais_e_repeticao/indentacao_e_blocos/exemplo.py
@@ -0,0 +1,17 @@
+
+# inicio do met do
+def sacar(valor):
+    saldo = 800
+
+    if saldo >= valor:
+        print("🏧 valor sacado!")
+        print("Retire o seu dinheiro na na boca do caixa 💰")
+    else:
+        print("saldo insuficiente❌")
+
+    print("Obrigado por ser nosso cliente,tenha um Bom Dia!! 😁")
+
+# fim do metodo
+
+
+sacar(1000)
