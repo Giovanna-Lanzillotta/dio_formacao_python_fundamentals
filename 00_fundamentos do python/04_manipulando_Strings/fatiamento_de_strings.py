f66f8b2124ac9ee939c9bec6b67cf1bb3e5eaae0
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-03-14 14:44:43 -0300

    fatiamento de string

---
+++ 00_fundamentos do python/04_manipulando_Strings/fatiamento_de_strings.py
@@ -0,0 +1,17 @@
+# FATIAMENTO DE STRING
+
+nome = "Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon"
+
+print(nome[0]) # P
+
+print(nome[:9]) # Pedro de
+
+print(nome[10:])
+
+print(nome[10:16])
+
+print(nome[10:16:2])
+
+print(nome[:])
+
+print(nome[::-1])
