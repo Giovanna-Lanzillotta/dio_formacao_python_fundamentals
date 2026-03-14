28ff5c393a6a2a544e73acc897491692f5f3b480
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-03-14 15:06:34 -0300

    strings multiplas linhas

---
+++ 00_fundamentos do python/04_manipulando_Strings/string_multiplas_linhas.py
@@ -0,0 +1,50 @@
+# STRING MÚLTIPLAS LINHAS
+
+# Strings triplas
+
+nome = "Giovanna"
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python 🐍
+"""
+
+print(mensagem)
+
+outra_mensagem = f'''
+   Olá meu nome é {nome},
+Eu estou aprendendo Python 🐍.
+     Essa mensagem tem diferentes recuos.
+'''
+
+print(outra_mensagem)
+
+
+menu = f"""
+======== MENU ========
+
+[1] Deposistar
+[2] Sacar
+[0] Sair
+
+======================
+     Obrigado por usar nosso sistema!!
+----------------------
+"""
+
+print(menu)
+
+
+teste = f"""
+⭐
+⭐⭐
+⭐⭐⭐
+⭐⭐⭐⭐
+⭐⭐⭐⭐⭐
+⭐⭐⭐⭐
+⭐⭐⭐
+⭐⭐
+⭐
+"""
+
+print(teste)
