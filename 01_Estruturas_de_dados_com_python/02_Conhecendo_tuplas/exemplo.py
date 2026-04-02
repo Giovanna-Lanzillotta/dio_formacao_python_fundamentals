85acc692d0a84a64395b94f9b183909069bae71b
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-04-01 22:10:25 -0300

    tuplas

---
+++ 01_Estruturas_de_dados_com_python/02_Conhecendo_tuplas/exemplo.py
@@ -0,0 +1,10 @@
+frutas = ("maçã", "laranja", "uva", "pera",)
+
+print(frutas[0]) # maçã
+print(frutas[2]) # uva
+print(frutas[3]) # pera
+
+#indices negativos
+print(frutas[-1]) # pera
+print(frutas[-3]) # laranja
+print(frutas[-2]) # uva
---
+++ 01_Estruturas_de_dados_com_python/02_Conhecendo_tuplas/tuplas_aninhadas.py
@@ -0,0 +1,14 @@
+# Tuplas aninhadas
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c")
+)
+
+print(matriz[0]) # (1, 'a', 2)
+print(matriz[0][0]) # 1
+print(matriz[0][-1]) # 2
+print(matriz[-1][-1]) # "c"
+print(matriz[-2][-1]) # 4
+print(matriz[-3][-3]) # 1
