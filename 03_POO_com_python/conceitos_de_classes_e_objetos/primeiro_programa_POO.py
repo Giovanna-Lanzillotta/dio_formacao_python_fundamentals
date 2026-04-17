7d57830603bad8fea8d36fc8045290d074344447
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-04-17 14:55:17 -0300

    bicicleta atualizações

--- 03_POO_com_python/conceitos_de_classes_e_objetos/primeiro_programa_POO.py
+++ 03_POO_com_python/conceitos_de_classes_e_objetos/primeiro_programa_POO.py
@@ -2,12 +2,13 @@
 # cor, modelo, ano e valor da bicicleta vendida. Uma bicicleta pode: buzina,parar e correr. Adicione esses comportamentos
 
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor, andando=False):
+    def __init__(self, cor, modelo, ano, valor, andando=False, aro=18):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
         self.andando = andando
+        self.aro = aro
     
     def buzinar(self):
         print("bi bi 📢")
@@ -21,6 +22,13 @@ class Bicicleta:
         print("correndo 🚴")
 
 
+    def __str__(self):
+        return f" 🚲 Bicicleta : cor:{self.cor}, modelo:{self.modelo}, ano:{self.ano}, valor:{self.valor} 🚲"
+
+
+    def __str__(self):
+        return f"{self.__class__.__name__} : {', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
+
 bicicleta_1 = Bicicleta("vermelha", "caloi", 2000, 599.99, True)
 bicicleta_2 = Bicicleta("azul", "caloi", 1998, 399.99)
 bicicleta_3 = Bicicleta("verde", "caloi", 2010, 699.99)
@@ -33,3 +41,5 @@ print(bicicleta_1.cor, bicicleta_1.modelo, bicicleta_1.ano, bicicleta_1.valor)
 print(bicicleta_2.cor, bicicleta_2.modelo)
 
 Bicicleta.buzinar(bicicleta_3)
+
+print(bicicleta_1)
