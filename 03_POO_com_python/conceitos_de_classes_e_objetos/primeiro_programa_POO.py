45fb829d3486370d961bb7d52b0f941836c6a3f1
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-04-16 21:38:13 -0300

    classe e objeto

---
+++ 03_POO_com_python/conceitos_de_classes_e_objetos/primeiro_programa_POO.py
@@ -0,0 +1,2 @@
+# João tem uma bicicletaria e gostaria de registrar as vendas de suas bicicletas. Crie um programa onde João informe:
+# cor, modelo, ano e valor da bicicleta vendida. Uma bicicleta pode: buzina,parar e correr. Adicione esses comportamentos
