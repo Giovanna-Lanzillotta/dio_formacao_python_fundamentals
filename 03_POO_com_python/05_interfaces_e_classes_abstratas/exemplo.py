763565fb6ab3659a8e447f03443b92c725cd3ca9
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-04-22 14:35:25 -0300

    interfaces e classes abstratas

---
+++ 03_POO_com_python/05_interfaces_e_classes_abstratas/exemplo.py
@@ -0,0 +1,29 @@
+# Variávesi de classe e Variáveis de instância
+
+class Estudante:
+    escola = "DIO"
+
+
+    def __init__(self, nome, numero):
+        self.nome = nome
+        self.numero = numero
+    
+
+    def __str__(self):
+        return f"{self.nome} ({self.numero} - {self.escola})"
+    
+
+def mostrar_valores(*objs):
+    for obj in objs:
+        print(obj)
+
+
+aluno_1 = Estudante("Guilherme", 1)
+aluno_2= Estudante("Giovanna", 2)
+mostrar_valores(aluno_1, aluno_2)
+
+# aluno_1.numero = 3
+Estudante.escola = "Python"
+aluno_3 = Estudante("Chappie",3)
+mostrar_valores(aluno_1, aluno_2,aluno_3)
+
