df19a6663954fc301cfa4402e637f6e3dfed2bda
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-04-22 10:56:56 -0300

    polimorfismo

---
+++ 03_POO_com_python/04_polimorfismo/polimorfismo.py
@@ -0,0 +1,15 @@
+# POLIMORFISMO
+# Significa muitas formas!
+
+# exemplo
+#string: conta o numero de caracteres
+texto = len("python")
+print(texto) # 6
+
+#lista: onta quantos elementos tem
+lista = len([10, 20, 30])
+print(lista) # 3
+
+#dicionário: conta pares de chave-valor
+dic = len({"nome": "Alice", "idade": 30}) # 2
+print(dic)
